7da39b55e094300fcbce1d507abfed949996340b
zlbandy <zlbandy@163.com>
2016-12-12 22:45:05 +0800

    网易云音乐api测试

---
+++ abcD/abcD/App_Code/MusicApi/MusicApis.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace abcD.App_Code.MusicApi
+{
+    public class MusicApis
+    {
+        public MusicApis()
+        {
+
+        }
+        public string Get_Music()
+        {
+            string url = "http://music.163.com/api/song/detail/?id=28377211&ids=%5B28377211%5D";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Referer = "http://music.163.com/";
+            request.Headers.Add("Cookie", "appver=1.5.2");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader sr = new StreamReader(response.GetResponseStream())) {
+
+                return sr.ReadToEnd().ToString();
+            }
+        }
+    }
+}
--- abcD/abcD/default.aspx.cs
+++ abcD/abcD/default.aspx.cs
@@ -1,17 +1,21 @@
-﻿using System;
+﻿using abcD.App_Code.MusicApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace abcD
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
+        public string abcD;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            MusicApis ma = new MusicApis();
+            abcD =   ma.Get_Music();
         }
     }
 }
