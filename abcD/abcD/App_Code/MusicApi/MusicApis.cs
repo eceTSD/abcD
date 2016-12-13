f8244a0d313d4fc7db4fc0bd3ff243d3489402c7
zlbandy <zlbandy@163.com>
2016-12-13 23:49:19 +0800

    网易云音乐api测试第二阶段

--- abcD/abcD/App_Code/MusicApi/MusicApis.cs
+++ abcD/abcD/App_Code/MusicApi/MusicApis.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Text;
 
 namespace abcD.App_Code.MusicApi
 {
@@ -15,15 +16,37 @@ namespace abcD.App_Code.MusicApi
         }
         public string Get_Music()
         {
-            string url = "http://music.163.com/api/song/detail/?id=28377211&ids=%5B28377211%5D";
+            string url = "http://music.163.com/api/search/pc";
+            string name = "周杰伦";
+            
+            string postData = "s="+ System.Web.HttpUtility.UrlEncode(name) + "&limit=20&type=1&offset=0";
+            
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
             request.Referer = "http://music.163.com/";
-            request.Headers.Add("Cookie", "appver=1.5.2");
+            request.Headers.Add("Cookie", "appver=2.0.2");
+            request.ContentType = "application/x-www-form-urlencoded";
+           
+            Stream myRequestStream = request.GetRequestStream();
+            StreamWriter sw = new StreamWriter(myRequestStream,Encoding.ASCII);
+            sw.Write(postData);
+            sw.Close();
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             using (StreamReader sr = new StreamReader(response.GetResponseStream())) {
 
                 return sr.ReadToEnd().ToString();
+                
             }
         }
+
+        public static string get_uft8(string unicodeString)
+        {
+            UTF8Encoding utf8 = new UTF8Encoding();
+            Byte[] encodedBytes = utf8.GetBytes(unicodeString);
+            String decodedString = utf8.GetString(encodedBytes);
+            return decodedString;
+        }
     }
+
+   
 }
